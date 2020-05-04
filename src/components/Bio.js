947d4ff7361e1085098d5b0b36a24f030eb73e92
Ying Lu <yinglu91@gmail.com>
2020-05-03 22:01:06 -0400

    blog update

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import { rhythm } from '../utils/typography'
 
-export default ({ settings }) => (
-  <div
-    style={{
-      display: 'flex',
-      marginBottom: rhythm(2.5),
-    }}
-  >
-    <img
-      src={settings.author_avatar.imgix_url}
-      alt={settings.author_name}
+const Bio = ({ settings }) => {
+  return (
+    <div
       style={{
-        marginRight: rhythm(1 / 2),
-        marginBottom: 0,
-        width: rhythm(2),
-        height: rhythm(2),
+        display: 'flex',
+        marginBottom: rhythm(2.5),
       }}
-    />
-    <div dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
-  </div>
-)
+    >
+      <img
+        src={settings.author_avatar.imgix_url}
+        alt={settings.author_name}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          width: rhythm(2),
+          height: rhythm(2),
+        }}
+      />
+      <div dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
+    </div>
+  )
+}
+
+export default Bio
+
+// site_title
+// author_name
+// author_bio
+// author_avatar {
+//   imgix_url
+// }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,124 +1,117 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { Link } from 'gatsby'
-import get from 'lodash/get'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import BackgroundImage from 'gatsby-background-image'
 
 import Bio from '../components/Bio'
 import Layout from '../components/layout'
 import { rhythm, scale } from '../utils/typography'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.cosmicjsPosts
-    const siteTitle = get(
-      this.props,
-      'data.cosmicjsSettings.metadata.site_title'
-    )
-    const author = get(this, 'props.data.cosmicjsSettings.metadata')
-    const location = get(this, 'props.location')
-    const { previous, next } = this.props.pageContext
+const BlogPostTemplate = ({ location, pageContext, data }) => {
+  const post = data.cosmicjsPosts
+  const siteTitle = data.cosmicjsSettings.metadata.site_title
+  const author = data.cosmicjsSettings.metadata
 
-    return (
-      <Layout location={location}>
-        <style>
-          {`
-          .post-content {
-            text-align: justify;
-          }
+  const { previous, next } = pageContext
+
+  return (
+    <Layout location={location}>
+      <style>
+        {`
+        .post-content {
+          text-align: justify;
+        }
+        .post-hero {
+          width: calc(100% + ${rhythm(8)});
+          margin-left: ${rhythm(-4)};
+          height: ${rhythm(18)};
+        }
+        @media (max-width: ${rhythm(32)}) {
           .post-hero {
-            width: calc(100% + ${rhythm(8)});
-            margin-left: ${rhythm(-4)};
-            height: ${rhythm(18)};
+            width: calc(100% + ${rhythm((3 / 4) * 2)});
+            margin-left: ${rhythm(-3 / 4)};
+            height: ${rhythm(13)};
           }
-          @media (max-width: ${rhythm(32)}) {
-            .post-hero {
-              width: calc(100% + ${rhythm((3 / 4) * 2)});
-              margin-left: ${rhythm(-3 / 4)};
-              height: ${rhythm(13)};
-            }
-          }
-        `}
-        </style>
-        <Helmet title={`${post.title} | ${siteTitle}`} />
-        <div
-          style={{
-            marginTop: rhythm(1.4),
-          }}
-        >
-          <Link to="/">← Back to Posts</Link>
-        </div>
-        <h1
-          style={{
-            marginTop: rhythm(1),
-          }}
-        >
-          {post.title}
-        </h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(0.6),
-            marginTop: rhythm(-0.6),
-          }}
-        >
-          {post.created}
-        </p>
-        <BackgroundImage
-          Tag="div"
-          className="post-hero"
-          fluid={post.metadata.hero.local.childImageSharp.fluid}
-          backgroundColor={`#007ACC`}
-          style={{
-            marginBottom: rhythm(0.6),
-          }}
-        />
-        <div
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: post.content }}
-        />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio settings={author} />
+        }
+      `}
+      </style>
+      <Helmet title={`${post.title} | ${siteTitle}`} />
+      <div
+        style={{
+          marginTop: rhythm(1.4),
+        }}
+      >
+        <Link to="/">← Back to Posts</Link>
+      </div>
+      <h1
+        style={{
+          marginTop: rhythm(1),
+        }}
+      >
+        {post.title}
+      </h1>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: 'block',
+          marginBottom: rhythm(0.6),
+          marginTop: rhythm(-0.6),
+        }}
+      >
+        {post.created}
+      </p>
+      <BackgroundImage
+        Tag="div"
+        className="post-hero"
+        fluid={post.metadata.hero.local.childImageSharp.fluid}
+        backgroundColor={`#007ACC`}
+        style={{
+          marginBottom: rhythm(0.6),
+        }}
+      />
+      <div
+        className="post-content"
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      />
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio settings={author} />
 
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0,
-          }}
-        >
-          {previous && (
-            <li>
-              <Link to={`posts/${previous.slug}`} rel="prev">
-                ← {previous.title}
-              </Link>
-            </li>
-          )}
+      <ul
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          listStyle: 'none',
+          padding: 0,
+        }}
+      >
+        {previous && (
+          <li>
+            <Link to={`posts/${previous.slug}`} rel="prev">
+              ← {previous.title}
+            </Link>
+          </li>
+        )}
 
-          {next && (
-            <li>
-              <Link to={`posts/${next.slug}`} rel="next">
-                {next.title} →
-              </Link>
-            </li>
-          )}
-        </ul>
-      </Layout>
-    )
-  }
+        {next && (
+          <li>
+            <Link to={`posts/${next.slug}`} rel="next">
+              {next.title} →
+            </Link>
+          </li>
+        )}
+      </ul>
+    </Layout>
+  )
 }
 
 export default BlogPostTemplate
 
-export const pageQuery = graphql`
+export const query = graphql`
   query BlogPostBySlug($slug: String!) {
     cosmicjsPosts(slug: { eq: $slug }) {
       id
