ad7d896bfdde5cf604b0c638ad7c2642e4ea8e84
Ying Lu <yinglu91@gmail.com>
2020-05-03 21:20:18 -0400

    add posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,55 +1,50 @@
-const each = require('lodash/each')
-const Promise = require('bluebird')
 const path = require('path')
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const indexPage = path.resolve('./src/pages/index.js')
+
+  // rename the index page from / to /posts
   createPage({
     path: `posts`,
-    component: indexPage,
+    component: path.resolve('./src/pages/index.js'),
   })
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allCosmicjsPosts(sort: { fields: [created], order: DESC }, limit: 1000) {
-              edges {
-                node {
-                  slug,
-                  title
-                }
-              }
-            }
+  const result = await graphql(
+    `
+      {
+        allCosmicjsPosts(
+          sort: { fields: [created], order: DESC }
+          limit: 1000
+        ) {
+          nodes {
+            slug
+            title
           }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      }
+    `
+  )
 
-        // Create blog posts pages.
-        const posts = result.data.allCosmicjsPosts.edges;
+  if (result.errors) {
+    console.log(result.errors)
+    return
+  }
 
-        each(posts, (post, index) => {
-          const next = index === posts.length - 1 ? null : posts[index + 1].node;
-          const previous = index === 0 ? null : posts[index - 1].node;
+  // Create blog posts pages.
 
-          createPage({
-            path: `posts/${post.node.slug}`,
-            component: blogPost,
-            context: {
-              slug: post.node.slug,
-              previous,
-              next,
-            },
-          })
-        })
-      })
-    )
+  const posts = result.data.allCosmicjsPosts.nodes
+  posts.forEach((post, index) => {
+    const next = index === posts.length - 1 ? null : posts[index + 1]
+    const previous = index === 0 ? null : posts[index - 1]
+
+    createPage({
+      path: `posts/${post.slug}`,
+      component: path.resolve('./src/templates/blog-post.js'),
+      context: {
+        slug: post.slug,
+        previous,
+        next,
+      },
+    })
   })
 }
